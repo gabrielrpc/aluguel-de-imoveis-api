1d1e46651a5dcaa05f5158c7d1a259315186e234
Gabriel <grpc15@outlook.com>
2025-06-26 23:23:02 -0300

    feat: adiciona controller para cadastro de usuário

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using aluguel_de_imoveis.Communication.Request;
+using aluguel_de_imoveis.Communication.Response;
+using aluguel_de_imoveis.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aluguel_de_imoveis.Controllers
+{
+    [Route("[controller]/CadastrarUsuario")]
+    [ApiController]
+    [ProducesResponseType(typeof(ResponseCadastrarUsuarioJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorMessegesJson), StatusCodes.Status400BadRequest)]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CadastrarUsuario(RequestUsuarioJson request)
+        {
+            var response = await _usuarioService.CadastrarUsuario(request);
+
+            return Created(string.Empty, response);
+        }
+    }
+}
