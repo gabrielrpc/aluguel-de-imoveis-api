c2aa32e6bafbbed133c007ce1be1a4b7ba780b97
Gabriel <grpc15@outlook.com>
2025-06-26 23:25:03 -0300

    feat: implementa IUsuarioRepository

---
+++ Repository/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,11 @@
+﻿using aluguel_de_imoveis.Models;
+
+namespace aluguel_de_imoveis.Repository.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<Usuario> CadastrarUsuario(Usuario usuario);
+
+        Task<bool> VerificarEmailExistente(string email);
+    }
+}
