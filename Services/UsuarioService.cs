470f0dd4d63390b7c74f8cac4e8f7a822d6bb190
Gabriel <grpc15@outlook.com>
2025-06-26 23:24:41 -0300

    feat: implementa a interface e adiciona validações

---
+++ Services/UsuarioService.cs
@@ -0,0 +1,50 @@
+﻿using aluguel_de_imoveis.Communication.Request;
+using aluguel_de_imoveis.Exceptions;
+using aluguel_de_imoveis.Models;
+using aluguel_de_imoveis.Repository.Interfaces;
+using aluguel_de_imoveis.Services.Interfaces;
+using aluguel_de_imoveis.Services.Validations;
+using FluentValidation.Results;
+
+namespace aluguel_de_imoveis.Services
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public UsuarioService(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        public async Task<Usuario> CadastrarUsuario(RequestUsuarioJson request)
+        {
+            var validator = new UsuarioValidations();
+
+            var result = validator.Validate(request);
+
+            if (result.IsValid == false)
+            {
+                var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+                throw new ErrorOnValidationException(errorMessages);
+            }
+
+            var emailExistente = await _usuarioRepository.VerificarEmailExistente(request.Email);
+
+            if(emailExistente)
+            {
+                throw new ErrorOnValidationException(new List<string> { "E-mail já registrado na plataforma." });
+            }
+
+            var novoUsuario = new Usuario
+            {
+                Nome = request.Nome,
+                Email = request.Email,
+                Senha = BCrypt.Net.BCrypt.HashPassword(request.Senha),
+                Tipo = request.Tipo
+            };
+
+            return await _usuarioRepository.CadastrarUsuario(novoUsuario);
+        }
+    }
+}
---
+++ Services/Validations/UsuarioValidations.cs
@@ -0,0 +1,21 @@
+﻿using aluguel_de_imoveis.Communication.Request;
+using FluentValidation;
+
+namespace aluguel_de_imoveis.Services.Validations
+{
+    public class UsuarioValidations : AbstractValidator<RequestUsuarioJson>
+    {
+        public UsuarioValidations()
+        {
+            RuleFor(request => request.Nome).NotEmpty().WithMessage("O nome é obrigatório.");
+            RuleFor(request => request.Email).EmailAddress().WithMessage("O email informado não é válido.");
+            RuleFor(request => request.Senha).NotEmpty().WithMessage("A senha é obrigatória.");
+            When(request => string.IsNullOrEmpty(request.Senha) == false, () =>
+            {
+                RuleFor(request => request.Senha.Length).GreaterThanOrEqualTo(6).WithMessage("A senha deve ter no mínimo 6 caracteres.");
+            });
+            RuleFor(request => request.Tipo).NotEmpty().WithMessage("O tipo é obrigatório.");
+
+        }
+    }
+}
