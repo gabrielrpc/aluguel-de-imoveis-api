7ee9cb1471b8e54a24ea82cbb9c5994a4727067f
Gabriel <grpc15@outlook.com>
2025-06-26 23:25:43 -0300

    feat: implementa IUsuarioRepository em UsuarioRepository

---
+++ Repository/UsuarioRepository.cs
@@ -0,0 +1,29 @@
+﻿using aluguel_de_imoveis.Infraestructure.DataAccess;
+using aluguel_de_imoveis.Models;
+using aluguel_de_imoveis.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace aluguel_de_imoveis.Repository
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly Context _context;
+
+        public UsuarioRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<Usuario> CadastrarUsuario(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+            return usuario;
+        }
+
+        public async Task<bool> VerificarEmailExistente(string email)
+        {
+            return await _context.Usuarios.AnyAsync(u => u.Email == email);
+        }
+    }
+}
