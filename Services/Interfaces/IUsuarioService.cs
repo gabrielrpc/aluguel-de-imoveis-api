b0dd50ddf1f71f7a01985069e013adbae8073e7d
Gabriel <grpc15@outlook.com>
2025-06-26 23:24:02 -0300

    feat: adiciona IUsuarioService

---
+++ Services/Interfaces/IUsuarioService.cs
@@ -0,0 +1,10 @@
+﻿using aluguel_de_imoveis.Communication.Request;
+using aluguel_de_imoveis.Models;
+
+namespace aluguel_de_imoveis.Services.Interfaces
+{
+    public interface IUsuarioService
+    {
+        Task<Usuario> CadastrarUsuario(RequestUsuarioJson request);
+    }
+}
