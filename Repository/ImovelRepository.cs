632c6309ad9fba6384bbdefd0de5a1cea4dfecb0
Gabriel <grpc15@outlook.com>
2025-06-27 23:02:50 -0300

    feat: cria imovel repository

---
+++ Repository/ImovelRepository.cs
@@ -0,0 +1,23 @@
+﻿using aluguel_de_imoveis.Infraestructure.DataAccess;
+using aluguel_de_imoveis.Models;
+using aluguel_de_imoveis.Repository.Interfaces;
+
+namespace aluguel_de_imoveis.Repository
+{
+    public class ImovelRepository : IImovelRepository
+    {
+        private readonly Context _context;
+
+        public ImovelRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<Imovel> CadastrarImovel(Imovel imovel)
+        {
+            _context.Imoveis.Add(imovel);
+            await _context.SaveChangesAsync();
+            return imovel;
+        }
+    }
+}
---
+++ Repository/Interfaces/IImovelRepository.cs
@@ -0,0 +1,9 @@
+﻿using aluguel_de_imoveis.Models;
+
+namespace aluguel_de_imoveis.Repository.Interfaces
+{
+    public interface IImovelRepository
+    {
+        Task<Imovel> CadastrarImovel(Imovel imovel);
+    }
+}
