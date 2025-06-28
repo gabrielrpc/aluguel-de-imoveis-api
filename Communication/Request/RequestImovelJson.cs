74e85c034a448b0fc2220ae66404ded8eff388ef
Gabriel <grpc15@outlook.com>
2025-06-27 23:00:48 -0300

    feat: cria classes de Comunicação (request) para Imovel e Endereço

---
+++ Communication/Request/RequestImovelJson.cs
@@ -0,0 +1,14 @@
+﻿namespace aluguel_de_imoveis.Communication.Request
+{
+    public class RequestImovelJson
+    {
+        public string Titulo { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public decimal ValorAluguel { get; set; }
+        public bool Disponivel { get; set; }
+        public int Tipo { get; set; }
+        public Guid UsuarioId { get; set; }
+
+        public RequestEnderecoJson Endereco { get; set; } = null!;
+    }
+}
