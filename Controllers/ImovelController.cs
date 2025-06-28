6110465ef131f1efbe5170471de382a85e954382
Gabriel <grpc15@outlook.com>
2025-06-27 23:00:10 -0300

    feat: adiciona controller Imovel com rota de cadastro

---
+++ Controllers/ImovelController.cs
@@ -0,0 +1,32 @@
+﻿using aluguel_de_imoveis.Communication.Request;
+using aluguel_de_imoveis.Communication.Response;
+using aluguel_de_imoveis.Services;
+using aluguel_de_imoveis.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aluguel_de_imoveis.Controllers
+{
+    [Route("imovel")]
+    [ApiController]
+    [Authorize]
+    public class ImovelController : ControllerBase
+    {
+        private readonly IImovelService _imovelService;
+
+        public ImovelController(IImovelService imovelService)
+        {
+            _imovelService = imovelService;
+        }
+
+        [HttpPost("cadastrar")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseErrorMessegesJson), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CadastrarImovel(RequestImovelJson request)
+        {
+            await _imovelService.CadastrarImovel(request);
+
+            return Created();
+        }
+    }
+}
