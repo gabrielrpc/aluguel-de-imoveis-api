3195c1c2c39c90dc44fc3c361c61517ef2061ea9
Gabriel <grpc15@outlook.com>
2025-06-27 23:02:22 -0300

    feat: cria service para imovel e adiciona validações para os dados do imovel e endereço

---
+++ Services/ImovelService.cs
@@ -0,0 +1,58 @@
+﻿using aluguel_de_imoveis.Communication.Request;
+using aluguel_de_imoveis.Exceptions;
+using aluguel_de_imoveis.Models;
+using aluguel_de_imoveis.Repository;
+using aluguel_de_imoveis.Repository.Interfaces;
+using aluguel_de_imoveis.Security;
+using aluguel_de_imoveis.Services.Interfaces;
+using aluguel_de_imoveis.Services.Validations;
+
+namespace aluguel_de_imoveis.Services
+{
+    public class ImovelService : IImovelService
+    {
+        private readonly IImovelRepository _imoveloRepository;
+
+        public ImovelService(IImovelRepository imovelRepository)
+        {
+            _imoveloRepository = imovelRepository;
+        }
+
+        public async Task<Imovel> CadastrarImovel(RequestImovelJson request)
+        {
+            request.Endereco.Cep = new string(request.Endereco.Cep.Where(char.IsDigit).ToArray());
+
+            var validator = new ImovelValidations();
+
+            var result = validator.Validate(request);
+
+            if (result.IsValid == false)
+            {
+                var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+                throw new ErrorOnValidationException(errorMessages);
+            }
+
+            var novoImovel = new Imovel
+            {
+                Titulo = request.Titulo,
+                Descricao = request.Descricao,
+                ValorAluguel = request.ValorAluguel,
+                Disponivel = request.Disponivel,
+                Tipo = request.Tipo,
+                Endereco = new Endereco
+                {
+                    Logradouro = request.Endereco.Logradouro,
+                    Numero = request.Endereco.Numero,
+                    Bairro = request.Endereco.Bairro,
+                    Cidade = request.Endereco.Cidade,
+                    Uf = request.Endereco.Uf,
+                    Cep = request.Endereco.Cep
+                },
+                UsuarioId = request.UsuarioId
+            };
+
+
+            return await _imoveloRepository.CadastrarImovel(novoImovel);
+        }
+    }
+}
---
+++ Services/Validations/ImovelValidations.cs
@@ -0,0 +1,26 @@
+﻿using aluguel_de_imoveis.Communication.Request;
+using FluentValidation;
+
+namespace aluguel_de_imoveis.Services.Validations
+{
+    public class ImovelValidations : AbstractValidator<RequestImovelJson>
+    {
+        public ImovelValidations()
+        {
+            RuleFor(request => request.Titulo).NotEmpty().WithMessage("O título é obrigatório.");
+            RuleFor(request => request.Descricao).NotEmpty().WithMessage("A descrição é obrigatória.");
+            RuleFor(request => request.ValorAluguel).GreaterThan(0).WithMessage("O valor do aluguel deve ser maior que zero.");
+            RuleFor(request => request.Tipo).GreaterThanOrEqualTo(0).WithMessage("O tipo do imóvel é obrigatório.");
+            RuleFor(request => request.Endereco).NotNull().WithMessage("O endereço é obrigatório.");
+            When(request => request.Endereco != null, () =>
+            {
+                RuleFor(request => request.Endereco.Logradouro).NotEmpty().WithMessage("A rua é obrigatória.");
+                RuleFor(request => request.Endereco.Numero).GreaterThanOrEqualTo(0).NotEmpty().WithMessage("O número é obrigatório.");
+                RuleFor(request => request.Endereco.Bairro).NotEmpty().WithMessage("O bairro é obrigatório.");
+                RuleFor(request => request.Endereco.Cidade).NotEmpty().WithMessage("A cidade é obrigatória.");
+                RuleFor(request => request.Endereco.Uf).NotEmpty().Length(2).WithMessage("O estado (UF) deve conter apenas as siglas.");
+                RuleFor(request => request.Endereco.Cep).Matches(@"^\d{8}$").NotEmpty().WithMessage("O CEP é obrigatório.");
+            });
+        }
+    }
+}
